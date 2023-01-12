226b59668ad71e15c8c1e801c50bc81c769d401d
stommy <thscharler@gmail.com>
2023-01-12 19:26:33 +0100

    m2 for throw_away_everything__again
    move ParseContext for ()

--- src/raw_context.rs
+++ src/raw_context.rs
@@ -1,3 +1,4 @@
+use crate::data_frame::undo_take_str_slice_unchecked;
 use crate::{Code, HoldContext, ParseContext, Span, SpanOffset};
 use std::error::Error;
 use std::marker::PhantomData;
@@ -23,6 +24,28 @@ impl<'s, C: Code> RawContext<'s, C> {
 impl<'s, C: Code> ParseContext<'s, C> for RawContext<'s, C> {
     fn original(&self, span: &Span<'s, C>) -> Span<'s, C> {
         Span::new_extra(self.span, span.extra)
+    }
+
+    fn enter(&self, _: C, _: &Span<'s, C>) {}
+
+    fn debug(&self, _: &Span<'s, C>, _: String) {}
+
+    fn info(&self, _: &Span<'s, C>, _: &'static str) {}
+
+    fn warn(&self, _: &Span<'s, C>, _: &'static str) {}
+
+    fn exit_ok(&self, _: &Span<'s, C>, _: &Span<'s, C>) {}
+
+    fn exit_err(&self, _: &Span<'s, C>, _: C, _: &dyn Error) {}
+}
+
+/// Null Context
+impl<'s, C: Code> ParseContext<'s, C> for () {
+    fn original(&self, span: &Span<'s, C>) -> Span<'s, C> {
+        unsafe {
+            let buf = undo_take_str_slice_unchecked(span.fragment(), span.location_offset());
+            Span::new_from_raw_offset(0, 1, buf, span.extra)
+        }
     }
 
     fn enter(&self, _: C, _: &Span<'s, C>) {}
